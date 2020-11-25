8ec77d222ce6e02a577a9edbb387457f6a75dee8
Dhina17 <dhinalogu@gmail.com>
2020-11-25 23:28:55 +0530

    CodeChef: Practice: Beginner: FSQRT

---
+++ CodeChef/Practice/Beginner/FSQRT/fsqrt.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        double n;
+        cin >> n;
+        cout << round(sqrt(n)) << endl;
+    }
+    return 0;
+}
