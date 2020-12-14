3b1b0324fd2cfd9849abed0ed95d2278814ecc56
Dhina17 <dhinalogu@gmail.com>
2020-12-14 23:50:22 +0530

    folder_gen: Add support for DSA problems

--- folder_gen.sh
+++ folder_gen.sh
@@ -1,11 +1,27 @@
 #!/usr/bin/sh
 
+# Get the option
+OPTION="${1:? Error: Give the option}"
+
 # Get link
-PROBLEM_LINK="${1:? Error: Give problem link}"
+PROBLEM_LINK="${2:? Error: Give problem link}"
 CC_CODE=${PROBLEM_LINK##*/}
 
+# Option handling
+case ${OPTION} in 
+    -d)
+    DIR=CodeChef/DataStructure/WarmUp/$CC_CODE
+    ;;
+    -p)
+    DIR=CodeChef/Practice/Beginner/$CC_CODE
+    ;;
+    *)
+    echo "${0}: usage: <option> <link>"
+    exit 1
+    ;;
+esac
+
 # Create directory
-DIR=CodeChef/Practice/Beginner/$CC_CODE
 mkdir -p $DIR
 
 ## Create a cpp file for the solution
@@ -15,4 +31,6 @@ touch $DIR/$file
 # Create README file
 READ_ME="## Problem link \n\n - $PROBLEM_LINK"
 echo -e $READ_ME > $DIR/README.md
-echo "Folder and file created successfully"
+
+# Notify
+echo "Folder and File created successfully"
