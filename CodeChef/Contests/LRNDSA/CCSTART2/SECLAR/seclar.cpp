9c3181a272f99b614e7c2de2214403d49accae08
Dhina17 <dhinalogu@gmail.com>
2021-04-16 23:17:58 +0530

    CodeChef: LRNDSA: CCSTART2: SECLAR

---
+++ CodeChef/Contests/LRNDSA/CCSTART2/SECLAR/seclar.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+
+#define ll long long int
+using namespace std;
+
+int main(){
+    ll a[3];
+    cin >> a[0] >> a[1] >> a[2];
+    sort(a, a+3);
+    cout << a[1] << endl;
+}
