81d10173f6b73e27db3541eb119b4f368e621467
Dhina17 <dhinalogu@gmail.com>
2021-04-19 23:40:13 +0530

    template: cpp: Add basic structure with test cases and add separate function for solution

--- templates/template.cpp
+++ templates/template.cpp
@@ -2,10 +2,21 @@
 
 using namespace std;
 
+void solve() {
+
+}
+
 int main() {
 
     //for faster input
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
+	// test case
+	int t;
+	cin >> t;
+	
+	while (t--) {
+		solve();
+	}
 }
