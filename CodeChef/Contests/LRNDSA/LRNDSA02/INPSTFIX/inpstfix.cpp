82345d207dbc6c646dfedc997a439ec3f98ef5f3
Dhina17 <dhinalogu@gmail.com>
2021-05-11 21:34:46 +0530

    CodeChef: Contest: LRNDSA02: INPSTFIX

---
+++ CodeChef/Contests/LRNDSA/LRNDSA02/INPSTFIX/inpstfix.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int getPreced(char op) {
+	switch(op) {
+		case '^':
+		   	return 3;
+		case '/':
+		   	return 2;
+		case '*':
+			return 2;
+		case '+':
+			return 1;
+		case '-':
+			return 1;
+		default:
+			return -1;
+	}
+}
+
+void solve() {
+	int n;
+	cin >> n;
+	string s;
+	cin >> s;
+	string postp = "";
+	stack<char> st;
+	for (int i = 0; i < n; i++) {
+		if (s[i] <= 'Z' && s[i] >= 'A') {
+			postp += s[i];
+		}else if (s[i] == '(') {
+			st.push(s[i]);
+		}else if (s[i] == ')') {
+			while (!st.empty() && st.top() != '(') {
+				postp += st.top();
+				st.pop();
+			}
+			st.pop();
+		} else {
+			while (!st.empty() && getPreced(s[i]) <= getPreced(st.top())) {
+				postp += st.top();
+				st.pop();
+			}
+			st.push(s[i]);
+		}
+	}
+	while (!st.empty()) {
+		postp += st.top();
+		st.pop();
+	}
+	cout << postp << endl;
+}
+
+int main() {
+
+    //for faster input
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+	// test case
+	int t;
+	cin >> t;
+	
+	while (t--) {
+		solve();
+	}
+}
